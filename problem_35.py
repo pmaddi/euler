4a8683b2c62626c340b0e8b3b3c78dd5e79b2975
Pranav Maddi <pranav.maddi@gmail.com>
2018-05-03 11:12:14 -0400

    P35

--- problem_35.py
+++ problem_35.py
@@ -1,19 +1,32 @@
+import numpy as np
+import math
+
+
+def rotations(n):
+    digits = int(math.log10(n)) + 1
+    for i in range(digits):
+        leading = n // (10  **  (digits - 1))
+        n = (n % (10  **  (digits - 1))) * 10 + leading
+        yield n
+
 def primes_lt(n):
     '''too slow'''
+    nums = np.zeros(n - 1)
+    nums[0] = 1
+    for i in range(2, int(n ** .5) + 1):
+        for j in range(2, (n // i) + 1):
+            if i * j < n:
+                nums[i * j - 1] = 1 # is nonprime
+    primes = set()
+    for i in range(len(nums)):
+        if not nums[i]:
+            primes.add(i + 1)
     out = []
-    for i in range(2, n):
-        prime = True
-        for d in out:
-            if i % d == 0:
-                prime = False
-                break
-        if prime:
+    for i in primes:
+        if all([j in primes for j in rotations(i)]):
             out.append(i)
-        if len(out) % 100 == 0:
-            print(len(out), max(out))
     return out
 
 if __name__ == '__main__':
-    pass
-
-
+    assert(list(rotations(123)) == [231, 312, 123])
+    print(len(primes_lt(10 ** 6)))
