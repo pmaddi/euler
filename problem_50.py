73f39e4c8329bb3dd888d044e2cd1e257460906c
Pranav Maddi <pranav.maddi@gmail.com>
2018-05-21 09:59:21 -0400

    P50

--- problem_50.py
+++ problem_50.py
@@ -1,15 +1,33 @@
 import numpy as np
-def build_arr_lt(n):
-    arr = np.repeat(0, n - 1)
-    arr[1 - 1] = 1 # 1 is considered composite
-    for i in range(2, n // 2 - 1):
-        if arr[i - 1] == 0: # if maybe prime
-            max_mult = (n - 1) // i
-            for multiples in range(2, max_mult + 1):
-                arr[i * multiples - 1] = 1
-    return arr
+from itertools import combinations
+
+def primes_lt(n):
+    '''too slow'''
+    nums = np.zeros(n - 1)
+    nums[0] = 1
+    for i in range(2, int(n ** .5) + 1):
+        for j in range(2, (n // i) + 1):
+            if i * j < n:
+                nums[i * j - 1] = 1 # is nonprime
+    primes = set()
+    for i in range(len(nums)):
+        if not nums[i]:
+            primes.add(i + 1)
+    return primes
+
 if __name__ == '__main__':
-    arr = build_arr_lt(10 ** 6)
-    import ipdb
-    ipdb.set_trace()
+    p_set = primes_lt(10 ** 6 + 1)
+    p_lst = sorted(list(p_set))
+    bsf = 0
+    bsf_cnt = 1
+    for i in range(len(p_lst)):
+        for j in range(i + bsf_cnt, len(p_lst) + 1):
+            sm = sum(p_lst[i: j])
+            if sm > 10 ** 6:
+                break
+            if sm in p_set:
+                if j - i > bsf_cnt:
+                    bsf_cnt = j - i
+                    bsf = sm
+    print(bsf)
 
