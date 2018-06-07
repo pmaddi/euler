c20670b177b79e2ca46b1e7a33f02a9682df7d35
Pranav Maddi <pranav.maddi@gmail.com>
2018-06-07 11:54:40 -0400

    P64

--- problem_64.py
+++ problem_64.py
@@ -1,44 +1,29 @@
-from math import sqrt#, log10
-
-def hsh(f, a):
-    return '{}, {}'.format(str(f)[:6], a[-3:])
-    # return '{}, {}'.format(str(f)[:6], 1)
+from math import sqrt
 
 def rep(v):
     s = sqrt(v)
-    initial = int(s)# % 10
+    initial = int(s)
     digits = [initial]
-    st = s - initial
     seen = {}
+    last_div = 1
+    last_top_right = -1 * initial
     for idx in range(1, 10**5):
-        h = hsh(st + digits[-1], digits)
+        h = (digits[idx - 1], last_div, last_top_right)
         sn = seen.get(h)
         if sn:
-            # print(digits)
-            # print(seen)
             return digits[(sn - 1):(idx - 1)]
         else:
             seen[h] = idx
-        if st == 0:
+        tmp_bottom = v - last_top_right ** 2
+        assert(tmp_bottom / last_div == tmp_bottom // last_div)
+        last_div = tmp_bottom // last_div
+        if last_div == 0:
             return []
-        nxt = int(1 / st)
-        digits.append(nxt)
-        st = 1 / st - nxt
-    print(digits)
-    print(seen)
+        val = int((s + -1 * last_top_right) / last_div)
+        digits.append(val)
+        last_top_right = -1 * last_top_right - last_div * val
     raise Exception('Couldnt find repeating for {}'.format(v))
-    # print("None. {}".format(v))
 
-# print(sum([len(rep(i)) % 2 == 1 for i in range(2, 10**4)]))
-# print(rep(83))
-# print(rep(76))
-# for i in range(70, 90):
-#     print(i, len(rep(i)))
-
-assert(sum([len(rep(i)) % 2 == 1 for i in range(2, 14)]) == 4)
-
-
-
-print(sum([len(rep(i)) % 2 == 1 for i in range(2, 10**4)]))
-
-# print(rep(285))
+if __name__ == '__main__':
+    assert(sum([len(rep(i)) % 2 == 1 for i in range(2, 14)]) == 4)
+    print(sum([len(rep(i)) % 2 == 1 for i in range(2, 10**4)]))
