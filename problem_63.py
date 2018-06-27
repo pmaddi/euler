06df531c1effe2afa31003cedf35843732ee5956
Pranav Maddi <pranav.maddi@gmail.com>
2018-06-27 11:23:01 -0400

    Move to main loop

--- problem_63.py
+++ problem_63.py
@@ -2,19 +2,20 @@ import math
 def digits(x):
     return int(math.log10(x)) + 1
 
-cnt = 0
-power = 1
-while True:
-    base = 1
+if __name__ == '__main__':
+    cnt = 0
+    power = 1
     while True:
-        num = base ** power
-        d = digits(num)
-        if d == power:
-            cnt += 1
-        if d > power:
+        base = 1
+        while True:
+            num = base ** power
+            d = digits(num)
+            if d == power:
+                cnt += 1
+            if d > power:
+                break
+            base += 1
+        power += 1
+        if power > 100:
             break
-        base += 1
-    power += 1
-    if power > 100:
-        break
-print(cnt)
+    print(cnt)
