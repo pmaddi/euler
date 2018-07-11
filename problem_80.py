fccf71733142c45a6b9d3e7b68fa27e6931b3e97
Pranav Maddi <pranav.maddi@gmail.com>
2018-07-11 12:14:41 -0400

    P80

--- problem_80.py
+++ problem_80.py
@@ -1,1 +1,16 @@
-print(2 ** .5)
+'''
+Violates the spirit of the problem. Thanks python for being the best
+'''
+import decimal
+
+if __name__ == '__main__':
+    decimal.getcontext().prec = 110
+    out = 0
+    for i in range(2, 100):
+        if int(i ** .5) ** 2 == i:
+            continue
+        d = decimal.Decimal(i) ** decimal.Decimal(.5)
+        val = str(d).replace('.', '')[:100]
+        v = sum([int(i) for i in list(val)])
+        out += v
+    print(out)
