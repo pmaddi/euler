3d2e0d058e6490e6a2a9c14c4c2b62d36899402d
Pranav Maddi <pranav.maddi@gmail.com>
2018-07-21 14:44:46 -0400

    P83

--- problem_83.py
+++ problem_83.py
@@ -20,12 +20,10 @@ def neighboars(i, j):
             (i, j - 1),
             (i, j + 1),
             (i + 1, j),
-            (i + 1, j),
+            (i - 1, j),
             ]
 
-def min_path(arr):
-    start_node = (0, 0)
-    target_node = (arr.shape[0] - 1, arr.shape[1] - 1)
+def min_path_to_from(arr, start_node, target_node):
     nodes = set()
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
@@ -47,29 +45,44 @@ def min_path(arr):
     ar = np.zeros(shape=arr.shape)
     for k, v in distances.items():
         ar[k] = v
-    print(ar)
-
     return distances[target_node]
+
+def min_path(arr):
+    start_node = (0, 0)
+    target_node = (arr.shape[0] - 1, arr.shape[1] - 1)
+    return min_path_to_from(arr, start_node, target_node)
 
 class Test83(unittest.TestCase):
     def test_min_path(self):
         arr = np.array([[131]]).T
-        self.assertEquals(min_path(arr), 131)
+        self.assertEqual(min_path(arr), 131)
         arr = np.array([[131, 1], [121, 2]]).T
-        self.assertEquals(min_path(arr), 134)
+        self.assertEqual(min_path(arr), 134)
         arr = np.array([
             [131, 201],
             [673, 96 ]])
-        self.assertEquals(min_path(arr), 428)
+        self.assertEqual(min_path(arr), 428)
         arr = np.array([
             [131, 201, 630, 537, 805],
             [673, 96, 803, 699, 732],
             [234, 342, 746, 497, 524],
             [103, 965, 422, 121, 37],
             [18, 150, 111, 956, 331]]).T
-        self.assertEquals(min_path(arr), 2297)
+        self.assertEqual(min_path(arr), 2297)
+
+    def test_main_path_to_from(self):
+        arr = np.array([
+            [131, 201,],
+            [673, 96, ],
+            [234, 342,],
+            [103, 965,],
+            [18, 150, ]]).T
+        self.assertEqual(min_path_to_from(arr, (0, 0), (1, 0)), 332)
+        self.assertEqual(min_path_to_from(arr, (0, 0), (1, 1)), 428)
+        self.assertEqual(min_path_to_from(arr, (0, 0), (1, 2)), 770)
+        self.assertEqual(min_path_to_from(arr, (0, 0), (0, 2)), 1004)
 
 if __name__ == '__main__':
-    unittest.main()
-    # mat = load('p083_matrix.txt')
-    # print(min_path(mat))
+    # unittest.main()
+    mat = load('p083_matrix.txt')
+    print(min_path(mat))
