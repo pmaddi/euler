c43f3be9ee29cced9f09ea41e1742ae573a6f812
Pranav Maddi <pranav.maddi@gmail.com>
2018-07-30 12:28:19 -0400

    P84

--- problem_84.py
+++ problem_84.py
@@ -1,49 +1,22 @@
-'''
-Alternative methods:
-    MCMC with metropolis-hastings
-    Direct simulation
-
-'''
-import numpy as np
 import pandas as pd
 import random
 
 COLS = 40
 STATES = 40
 
-def dice_odds(size):
-    se = pd.Series([])
-    for i in range(size):
-        for j in range(size):
-            v = i + j + 2
-            se[v] = se.get(v, 0) + 1
-    return se / se.sum()
+GO = 0
+C1 = 11
+E3 = 24
+H2 = 39
+R1 = 5
+G2J = 30
 
-def test_dice_odds():
-    assert(dice_odds(6)[2] == 1/36)
-    assert(dice_odds(6)[12] == 1/36)
+JAIL = 10
+CC = [2, 17, 33]
+CH = [7, 22, 36]
+RAIL = [5, 15, 25, 35]
+UTILITY = [12, 28]
 
-def just_dice_matrix(dice_size):
-    mat = np.zeros(shape=(COLS, COLS))
-    mat = np.mat(mat)
-    odds = dice_odds(dice_size)
-    for i in range(COLS):
-        for offset, prob in odds.iteritems():
-            mat[i, (i + offset) % COLS] = prob
-    return mat
-
-def stationary(mat):
-    powr = mat
-    for i in range(1000):
-        powr = powr * mat
-    return np.array(powr)[0]
-
-def most_probable_str(stat):
-    st = sorted(list(enumerate(stat)), key=lambda x: x[0], reverse=True)
-    out = ''
-    for i, _ in st[:3]:
-        out += str(i).zfill(2)
-    return out
 
 def most_probable_str_series(stat):
     st = sorted(stat.items(), key=lambda x: x[1], reverse=True)
@@ -52,26 +25,8 @@ def most_probable_str_series(stat):
         out += str(i).zfill(2)
     return out
 
-def p_84(sides):
-    mat = just_dice_matrix(6)
-    stab = stationary(mat)
-    return most_probable_str(stab)
-
 def roll_die(n):
     return random.sample(range(1, n + 1), 1)[0]
-
-GO = 0
-C1 = 11
-E3 = 24
-H2 = 39
-R1 = 5
-
-JAIL = 10
-G2J = 30
-CC = [2, 17, 33]
-CH = [7, 22, 36]
-RAIL = [5, 15, 25, 35]
-UTILITY = [12, 28]
 
 def all_doubles(lst: [(int, int)]) -> bool:
     if len(lst) != 3:
@@ -139,7 +94,6 @@ def just_play(die):
             options = [to_id] * 14 + [to_constant(GO), to_constant(JAIL)]
             fn = random.sample(options, 1)[0]
             state = fn(state)
-
 
     se = pd.Series(counts)
     return se / se.sum()
