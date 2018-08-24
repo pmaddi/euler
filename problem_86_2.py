013315816b0c7fad9183ff738842f0f2a5d1bbfe
Pranav Maddi <pranav.maddi@gmail.com>
2018-08-24 20:09:17 +0000

    idk

--- problem_86_2.py
+++ problem_86_2.py
@@ -3,5 +3,6 @@ generate side lengths (how many to generate?)
     break up both legs
         add a count to the right bucket
         dont store the cuboid sides because it should be unique? (is this true?)
+            this kinda hinge on this capability :/
 
 '''
