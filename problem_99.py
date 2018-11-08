f3e92798b05d6707df22d7aa74c94ba8e91a3590
Pranav Maddi <pranav.maddi@gmail.com>
2018-11-08 20:55:17 +0000

    p99

--- problem_99.py
+++ problem_99.py
@@ -1,21 +1,33 @@
 from pathlib import Path
-
-def make_exp(base, exp):
-    return base ** exp
+from math import log
 
 def argmax(lst):
     msf = None
     msf_idx = 0
     for idx, i in enumerate(lst):
-        if msf is None or i > msf:
+        if msf is None or cmp(i, msf) == -1:
             msf = i
             msf_idx = idx
-        print('>>>', idx, msf_idx)
     return msf_idx
+
+def cmp(n1, n2):
+    b1, e1 = n1
+    b2, e2 = n2
+    st = (e1 * log(b1)) / (e2 * log(b2))
+    if st == 1:
+        return 0
+    elif st < 1:
+        return 1
+    else:
+        return -1
+
+def test_cmp():
+    assert cmp((2, 11), (3, 7)) == 1
+    assert cmp((632382, 518061), (519432, 525806)) == -1
+    assert cmp((519432, 525806), (632382, 518061)) == 1
 
 if __name__ == '__main__':
     r  = Path('p099_base_exp.txt').read_text()
-    s = r.split()[:100]
-    q = [[int(j) for j in i.split(',')] for i in s]
-    q1 = (make_exp(*i) for i in q)
-    print(argmax(q1) + 1)
+    s = r.split()
+    q = ([int(j) for j in i.split(',')] for i in s)
+    print(argmax(q) + 1)
