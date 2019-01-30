446d9ec403d441b5585d14eabc42cc21cd638b29
Pranav Maddi <pranav.maddi@gmail.com>
2019-01-29 20:51:07 -0800

    wip98

--- problem_98.py
+++ problem_98.py
@@ -14,4 +14,37 @@ mapping works??
 from pathlib import Path
 
 words = Path('p098_words.txt').read_text().replace('"', '').split(',')
-print(max(len(w) for w in words))
+a = {}
+
+def word_anagrams():
+    # Word anagrams
+    for w in words:
+        k = ''.join(sorted(w))
+        v = a.get(k, [])
+        v += [w]
+        a[k] = v
+    b = {}
+    mlen = 0
+    for k, v in a.items():
+        if len(v) > 1:
+            if len(v[0]) == 9:
+                print(v)
+
+def digit_anagrams():
+    a = {}
+    for i in range(int((10 ** 8) ** .5), int((10 ** 9) ** .5) + 1):
+        sq = i ** 2
+        sqstr = ''.join(sorted(str(sq)))
+        v = a.get(sqstr, [])
+        v += [sqstr]
+        a[sqstr] = v
+    cnt = 0
+    for k, v in a.items():
+        if len(v) > 1:
+            if len(v[0]) == 9:
+                if len(set(v[0])) == 9:
+                    print(v)
+                    cnt += 1
+    print(cnt)
+if __name__ == '__main__':
+    digit_anagrams()
