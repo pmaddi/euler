3bfd5e532695f12577a1f7aa086ec1c8c98f720b
Your Name <pranav.maddi@gmail.com>
2019-02-16 10:54:55 -0500

    wip

--- problem_98.py
+++ problem_98.py
@@ -13,38 +13,77 @@ mapping works??
 
 from pathlib import Path
 
-words = Path('p098_words.txt').read_text().replace('"', '').split(',')
-a = {}
+
+DIGITS = 5
+
+
+def repositionings(a, b):
+    '''The new indexes of elements in a mapped to b'''
+    out = ""
+    for c in a:
+        out = out + str(b.index(c))
+    return out
+
+
+def test_repositionings():
+    assert(repositionings('abc', 'cba') == '210')
+    assert(repositionings('', '') == '')
+
+
+def identity(x):
+    return x
+
+
+def anagrams(items, keyfn=identity, valuefn=identity):
+    a = {}
+    for i in items:
+        key = ''.join(sorted(keyfn(i)))
+        v = a.get(key, [])
+        v += [valuefn(i)]
+        a[key] = v
+    return {k: v for k, v in a.items() if len(v) > 1}
+
+
+def test_anagrams():
+    assert(anagrams(['abc', 'a', 'd']) == {})
+    assert(len(anagrams(['abc', 'cba', 'd'])) == 1)
+
 
 def word_anagrams():
     # Word anagrams
-    for w in words:
-        k = ''.join(sorted(w))
-        v = a.get(k, [])
-        v += [w]
-        a[k] = v
-    b = {}
-    mlen = 0
+    words = Path('p098_words.txt').read_text().replace('"', '').split(',')
+    a = anagrams(words)
     for k, v in a.items():
-        if len(v) > 1:
-            if len(v[0]) == 9:
-                print(v)
+        if len(v[0]) != DIGITS:
+            continue
+        for i in v:
+            for j in v:
+                if i == j:
+                    continue
+                yield(i, j, repositionings(i, j))
+
 
 def digit_anagrams():
-    a = {}
-    for i in range(int((10 ** 8) ** .5), int((10 ** 9) ** .5) + 1):
-        sq = i ** 2
-        sqstr = ''.join(sorted(str(sq)))
-        v = a.get(sqstr, [])
-        v += [sqstr]
-        a[sqstr] = v
-    cnt = 0
+    rng = range(int((10 ** (DIGITS - 1)) ** .5), int((10 ** DIGITS) ** .5) + 1)
+    a = anagrams(rng, keyfn=lambda x: str(x ** 2), valuefn=lambda x: x ** 2)
     for k, v in a.items():
-        if len(v) > 1:
-            if len(v[0]) == 9:
-                if len(set(v[0])) == 9:
-                    print(v)
-                    cnt += 1
-    print(cnt)
+        if len(str(v[0])) != DIGITS:
+            continue
+        for i in v:
+            for j in v:
+                if i == j:
+                    continue
+                yield(i, j, repositionings(str(i), str(j)))
+
+
+def comb():
+    dig = list(digit_anagrams())
+    wor = list(word_anagrams())
+    for w in wor:
+        for d in dig:
+            if w[2] == d[2]:
+                print(w, d)
+
+
 if __name__ == '__main__':
-    digit_anagrams()
+    comb()
