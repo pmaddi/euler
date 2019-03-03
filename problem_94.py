4e243dfe2ade3b46f2bb90ce03cd6df6e9d5bce2
Your Name <pranav.maddi@gmail.com>
2019-03-02 19:35:59 -0500

    P94

--- problem_94.py
+++ problem_94.py
@@ -1,72 +1,44 @@
-# # Too slow
-# from math import sqrt
-#
-# def area(a, b, c):
-#     p = (a + b + c) * .5
-#     return sqrt(p * (p - a) * (p - b) * (p - c))
-#
-# def test_area():
-#     assert area(5, 5, 6) == 12
-#
-# def int_ish(f):
-#     return f == int(f)
-#
-# def test_int_ish():
-#     assert not int_ish(1.00001)
-#     assert not int_ish(.9999)
-#     assert int_ish(sqrt(4))
-#     assert int_ish(sqrt(1234 ** 2))
-#
-# if __name__ == '__main__':
-#     sm = 0
-#     for dbl_side_length in range(2, 10 ** 9):
-#         for other in (dbl_side_length - 1, dbl_side_length + 1):
-#             p = dbl_side_length * 2 + other
-#             if p > 10 ** 9:
-#                 print(sm)
-#                 exit()
-#             a = area(dbl_side_length, dbl_side_length, other)
-#             if int_ish(a):
-#                 sm += p
-#     print(sm)
 '''
-peremiter = 3s - 1 or 3s + 1
+This is very slow but techincally works.
 
-p = (3s - 1) * .5 or (3s + 1) * .5
-area = sqrt(((3s - 1) * .5) * (((3s - 1) * .5) - s)**2 * (((3s - 1) * .5) - (s - 1)))
-
-and find where the area value is an int?
-is there a diophantine for this?
+Could be improved by genreating pythagrean triples
 '''
 
-from math import sqrt
 
-def area(a, b, c):
-    p = (a + b + c) * .5
-    return sqrt(p * (p - a) * (p - b) * (p - c))
+def square(x):
+    return int(x ** .5) ** 2 == x
 
-def test_area():
-    assert area(5, 5, 6) == 12
 
-def int_ish(f):
-    return f == int(f)
+def opt1(s):
+    right = (s // 2) ** 2
+    l1 = (s + 1) ** 2
+    if square(l1 - right):
+        p = 2 * (s + 1) + s
+        if p > 10 ** 9:
+            return
+        return p
 
-def test_int_ish():
-    assert not int_ish(1.00001)
-    assert not int_ish(.9999)
-    assert int_ish(sqrt(4))
-    assert int_ish(sqrt(1234 ** 2))
+
+def opt2(s):
+    right = (s // 2) ** 2
+    l2 = (s - 1) ** 2
+    if square(l2 - right):
+        p = 2 * (s - 1) + s
+        if p > 10 ** 9:
+            return
+        return p
+
 
 if __name__ == '__main__':
     sm = 0
-    for dbl_side_length in range(2, 10 ** 6):
-        for other in (dbl_side_length - 1, dbl_side_length + 1):
-            p = dbl_side_length * 2 + other
-            if p > 10 ** 9:
-                print(sm)
-                exit()
-            a = area(dbl_side_length, dbl_side_length, other)
-            if int_ish(a):
-                print(dbl_side_length, dbl_side_length, other, a)
-                sm += p
+    for s in range(3, 10 ** 9):
+        if s % 2 == 1:
+            continue
+        right = (s // 2) ** 2
+        o1 = opt1(s)
+        if o1 is not None:
+            sm += o1
+        o2 = opt2(s)
+        if o2 is not None:
+            sm += o2
     print(sm)
