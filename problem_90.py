9cfc5730bec2ca65655592d16f60de47559b868a
Your Name <pranav.maddi@gmail.com>
2019-03-20 11:21:47 -0700

    P90!!!!
    Finally did the first 100. Only took a frickin year lmao

--- problem_90.py
+++ problem_90.py
@@ -1,7 +1,9 @@
 '''
 Go thru all dicings
 
-Oops! Didn't notice values on the cubes needn't be unique.
+The faces are unique.
+
+
 '''
 PR = (
         (0, 1),
@@ -44,9 +46,11 @@ def test_valid():
     assert not valid((0, 0, 0, 0, 0, 0), (1, 2, 3, 4, 8, 9))
 
 
-if __name__ == '__main__':
+def all_dice():
+    """Produces all options for dice, where each face is unique."""
     lst = set()
     for i in range(2 ** 10):
+        # Include or exclude each digit.
         v = bin(i)[2:].zfill(10)
         st = []
         for c, x in enumerate(v):
@@ -54,14 +58,20 @@ if __name__ == '__main__':
                 st.append(c)
         if len(st) == 6:
             lst.add(tuple(st))
+    return lst
+
+
+def pairs(lst):
     lst_two = set()
     for i in lst:
         for j in lst:
             lst_two.add(tuple(sorted((i, j))))
+    return lst_two
+
+
+if __name__ == '__main__':
     cnt = 0
-    for i in lst:
-        for j in lst:
-            print(i, j, valid(i, j))
-            if valid(i, j):
-                cnt += 1
+    for i, j in pairs(all_dice()):
+        if valid(i, j):
+            cnt += 1
     print(cnt)
