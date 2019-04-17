1082e7413524b6f48cf69fc6937378e721941e19
Your Name <pranav.maddi@gmail.com>
2019-04-17 12:13:33 -0700

    p106

--- problem_106.py
+++ problem_106.py
@@ -1,29 +1,3 @@
-'''
-3 4 5 6
-cmp 3,4 6
-cmp 3 4
-cmp 4 5
-cmp 5 6
-You need to compare things that arent enforced by the increasing ordering and
-more is greater!
-
-prop: if a subset is unequal size, then we dont need to check (i)
-prop: if subests are of size 1 then no need to check because strictly
-increasing
-if there's no "overlap", then no need to check
-
-aabb
-abba
-abab -> not tested becuase each element of a has a strictly greater than in the
-other set.
-
-3678
-
-Go through each of the subset pairs and check whether each elem has a strictly
-greater than in the other set. If not, then needs to be checked.
-
-'''
-
 from itertools import combinations, chain
 
 
@@ -47,10 +21,53 @@ def subsets(n):
 
 
 def test_subsets():
-    assert (len(subsets(4))) == 25
-    assert (len(subsets(7))) == 966
-    assert (len(subsets(12))) == 261625
+    assert len(subsets(4)) == 25
+    assert len(subsets(7)) == 966
+    assert len(subsets(12)) == 261625
+
+
+def check(a, b):
+    '''Whether it needs to be checked.'''
+    if len(a) != len(b):
+        return False
+    if len(a) == 1:
+        return False
+    if a[0] > b[0]:
+        gr = a
+        le = b
+    else:
+        gr = b
+        le = a
+    # If all the lt values are covered by a greater value, don't check. But if
+    # one if them is covered, must check.
+    for idx in range(len(le)):
+        if le[idx] >= gr[idx]:
+            return True
+    return False
+
+
+def test_check():
+    assert check([0, 3], [1, 2])
+    assert check([1, 2], [0, 3])
+    assert not check([0, 1], [2, 3])
+    assert not check([0, 2], [1, 3])
+    assert not check([1, 3], [0, 2])
+    assert not check([0], [1])
+    assert not check([2], [1, 2])
+
+
+def checks_needed(n):
+    cnt = 0
+    for a, b in subsets(n):
+        if check(a, b):
+            cnt += 1
+    return cnt
+
+
+def test_checks_needed():
+    assert checks_needed(4) == 1
+    assert checks_needed(7) == 70
 
 
 if __name__ == '__main__':
-    print(subsets(4))
+    print(checks_needed(12))
