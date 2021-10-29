b09e6c632fccc0a400963361be80db53dd2c6ea4
Pranav Maddi <pranav.maddi@gmail.com>
2021-10-29 13:52:46 -0600

    wip

--- problem_126.py
+++ problem_126.py
@@ -6,6 +6,7 @@ def create(d):
                 l1.add((i, j, k))
     return l1
 
+
 def gen_next(vol):
     """Returns total vol and last layer."""
     l2 = set()
@@ -17,14 +18,30 @@ def gen_next(vol):
     l2 = l2 - vol
     return vol | l2, l2 - vol
 
-v = create([1,2,3])
-l1 = v
-v, l2 = gen_next(v)
-v, l3 = gen_next(v)
-v, l4 = gen_next(v)
-v, l5 = gen_next(v)
-print(len(l1))
-print(len(l2))
-print(len(l3))
-print(len(l4))
-print(len(l5))
+
+def find():
+    i, j, k = 1
+    while(True):
+        
+    seen = []
+    for i in range(1, 10):
+        for j in range(i, 10):
+            for k in range(j, 10):
+                dims = [i, j, k]
+                v = create(dims)
+                seen.append((dims, len(gen_next(v)[1])))
+    print(seen)
+
+find()
+
+# v = create([1,2,3])
+# l1 = v
+# v, l2 = gen_next(v)
+# v, l3 = gen_next(v)
+# v, l4 = gen_next(v)
+# v, l5 = gen_next(v)
+# print(len(l1))
+# print(len(l2))
+# print(len(l3))
+# print(len(l4))
+# print(len(l5))
